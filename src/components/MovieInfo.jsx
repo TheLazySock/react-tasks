d9e0f215ca256595ef0a85ea3e6e1a628f1ff1b6
Uladzislau Piatrenka <Uladzislau_Piatrenka@epam.com>
2018-05-18 10:51:02 +0300

    movie page added also with movie info component

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const MovieInfo = () => {
+const MovieInfo = (props) => {
+    console.log(props);
+    const movie = props.movie;
+    const releaseDateYear = new Date(movie.release_date).getFullYear();
+
+    const genresString = movie.genres.join(' & ');
+
     return (
-        <div>This is MovieInfo component</div>
-    )
+        <div className="movie">
+            <img src={movie.poster_path} className="movie-image" />
+            <div className="movie-details">
+                <div className="movie-title">{movie.title}</div>
+                <div className="movie-rating">{movie.vote_average}</div>
+                <div className="movie-genre">{genresString}</div>
+                <div className="movie-numbers">{releaseDateYear}</div>
+                <div className="movie-numbers">{movie.runtime} min</div>
+                <div className="scrollable">
+                    <div className="movie-description">{movie.overview}</div>
+                    {/* <div className="movie-role">Director: {props.director}</div> */}
+                    {/* <div className="movie-role">Cast List: {castList}</div> */}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default MovieInfo;
--- src/containers/MoviePage.jsx
+++ src/containers/MoviePage.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const MoviePage = () => {
-    return (
-        <div>This is MoviePage component</div>
-    )
+import MovieInfo from '../components/MovieInfo';
+import SimilarGenreContainer from './SimilarGenreContainer';
+import BackdropContainer from '../components/BackdropContainer';
+
+import { searchMovies } from '../redux/actions';
+
+class MoviePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movie: {
+                "id": 962,
+                "title": "The Gold Rush",
+                "tagline": "The World's Greatest Laughing Picture!",
+                "vote_average": 7.8,
+                "vote_count": 411,
+                "release_date": "1925-06-25",
+                "poster_path": "https://image.tmdb.org/t/p/w500/eQRFo1qwRREYwj47Yoe1PisgOle.jpg",
+                "overview": "A lone prospector ventures into Alaska looking for gold. He gets mixed up with some burly characters and falls in love with the beautiful Georgia. He tries to win her heart with his singular charm.",
+                "budget": 923000,
+                "revenue": 2500000,
+                "genres": [
+                    "Adventure",
+                    "Comedy",
+                    "Drama"
+                ],
+                "runtime": 95
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <BackdropContainer>
+                    <MovieInfo movie={this.state.movie} />
+                </BackdropContainer>
+                <SimilarGenreContainer />
+            </div>
+        );
+    }
 }
 
-export default MoviePage;
+export default connect(null, { searchMovies })(MoviePage);
