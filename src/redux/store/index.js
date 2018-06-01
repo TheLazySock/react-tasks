0fd2d9416eca44ae00b3c1a3dbe548fdf4d0252e
Uladzislau Piatrenka <Uladzislau_Piatrenka@epam.com>
2018-06-01 15:38:23 +0300

    first steps of redux storage restoration

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from '../reducers';
 
+const config = {
+    key: 'storage',
+    storage
+}
+
+const persistedReducer = persistReducer(config, rootReducer)
+
 // const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
 
+// export default { store, persistor };
 export default store;
